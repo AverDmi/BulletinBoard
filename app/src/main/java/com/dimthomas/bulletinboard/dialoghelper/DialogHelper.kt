c38a22fd85b62bb545b068742d4bdd08ed337d03
Dmitriy Averyanov <dimthomas1989@gmail.com>
2024-01-06 17:37:11 +0300

    Added reset password

--- app/src/main/java/com/dimthomas/bulletinboard/dialoghelper/DialogHelper.kt
+++ app/src/main/java/com/dimthomas/bulletinboard/dialoghelper/DialogHelper.kt
@@ -1,6 +1,8 @@
 package com.dimthomas.bulletinboard.dialoghelper
 
 import android.app.AlertDialog
+import android.view.View
+import android.widget.Toast
 import com.dimthomas.bulletinboard.MainActivity
 import com.dimthomas.bulletinboard.R
 import com.dimthomas.bulletinboard.accounthelper.AccountHelper
@@ -14,33 +16,58 @@ class DialogHelper(private val activity: MainActivity) {
         val builder = AlertDialog.Builder(activity)
         val binding = SignDialogBinding.inflate(activity.layoutInflater)
         builder.setView(binding.root)
+        setDialogState(index, binding)
 
+        val dialog = builder.create()
+
+        binding.signUpInBtn.setOnClickListener {
+           setOnClickSignUpIn(index, binding, dialog)
+        }
+
+        binding.forgetPasswordBtn.setOnClickListener {
+            setOnClickResetPassword(binding, dialog)
+        }
+
+        dialog.show()
+    }
+
+    private fun setOnClickResetPassword(binding: SignDialogBinding, dialog: AlertDialog?) {
+        if (binding.signEmailEd.text.isNotEmpty()) {
+            activity.mAuth.sendPasswordResetEmail(binding.signEmailEd.text.toString()).addOnCompleteListener { task -> 
+                if (task.isSuccessful) {
+                    Toast.makeText(activity, R.string.email_reset_password_was_sent, Toast.LENGTH_SHORT).show()
+                }
+            }
+            dialog?.dismiss()
+        } else {
+            binding.dialogMessageTv.visibility = View.VISIBLE
+        }
+    }
+
+    private fun setOnClickSignUpIn(index: Int, binding: SignDialogBinding, dialog: AlertDialog?) {
+        dialog?.dismiss()
+        if (index == SIGN_UP_STATE) {
+            accHelper.signUpWithEmail(
+                binding.signEmailEd.text.toString(),
+                binding.signPasswordEd.text.toString()
+            )
+        } else {
+            accHelper.signInWithEmail(
+                binding.signEmailEd.text.toString(),
+                binding.signPasswordEd.text.toString()
+            )
+        }
+    }
+
+    private fun setDialogState(index: Int, binding: SignDialogBinding) {
         if (index == SIGN_UP_STATE) {
             binding.signTitleTv.text = activity.resources.getString(R.string.sign_up)
             binding.signUpInBtn.text = activity.resources.getString(R.string.sign_up_action)
         } else {
             binding.signTitleTv.text = activity.resources.getString(R.string.sign_in)
             binding.signUpInBtn.text = activity.resources.getString(R.string.sign_in_action)
+            binding.forgetPasswordBtn.visibility = View.VISIBLE
         }
-
-        val dialog = builder.create()
-
-        binding.signUpInBtn.setOnClickListener {
-            dialog.dismiss()
-            if (index == SIGN_UP_STATE) {
-                accHelper.signUpWithEmail(
-                    binding.signEmailEd.text.toString(),
-                    binding.signPasswordEd.text.toString()
-                )
-            } else {
-                accHelper.signInWithEmail(
-                    binding.signEmailEd.text.toString(),
-                    binding.signPasswordEd.text.toString()
-                )
-            }
-        }
-
-        dialog.show()
     }
 
     companion object {
